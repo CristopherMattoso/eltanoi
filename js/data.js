4dd03fcdda0187d91f1e297b57807c06d8eccffb
CristopherMattoso <75039825+CristopherMattoso@users.noreply.github.com>
2026-01-08 15:28:04 -0300

    Create data.js
    criando layout do wiki

---
+++ js/data.js
@@ -0,0 +1,14 @@
+const pages = [
+  {
+    title: "Início",
+    file: "inicio.html"
+  },
+  {
+    title: "História",
+    file: "historia.html"
+  },
+  {
+    title: "Geografia",
+    file: "geografia.html"
+  }
+];
